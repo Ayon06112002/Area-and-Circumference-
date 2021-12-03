56be36b2c2fca48a69d57414e7a8c9453cd4452d
Ayon06112002 <89964596+Ayon06112002@users.noreply.github.com>
2021-12-03 18:57:44 +0530

    Add files via upload

---
+++ 2.cpp
@@ -0,0 +1,55 @@
+// Menu driven program to calculate the area and circumference of a circle.
+#include <iostream>
+#include <cmath>
+using namespace std;
+const double pi = 3.147;
+
+void area(double r)
+{
+    // cout<<"Enter circumference : ";
+    double area;
+    area = pi * pow(r, 2);
+    cout << area << "m^2" << endl;
+}
+void circumference(double r)
+{
+    // cout<<"Enter  : ";
+    double circumference;
+    circumference = 2 * pi * r;
+    cout << circumference << "m" << endl;
+}
+int main()
+{
+    cout << "Enter radius of the circle : ";
+    double r;
+    cin >> r;
+    bool t = true;
+
+    while (t == true)
+    {
+        cout << "What do you want to calculate : " << endl;
+        cout << "1. AREA " << endl;
+        cout << "1. CIRCUMFERENCE " << endl;
+        int choice;
+        cout << "Choice: ";
+        cin >> choice;
+        switch (choice)
+        {
+        case 1:
+            area(r);
+            break;
+        case 2:
+            circumference(r);
+            break;
+        default:
+            cout << "Invalid choice" << endl;
+        }
+        cout << "Do you wish to continue (press n or N for termination: " << endl;
+        char ch;
+        cin >> ch;
+        if (ch == 'n' || ch == 'N')
+            t = false;
+    }
+
+    return 0;
+}
